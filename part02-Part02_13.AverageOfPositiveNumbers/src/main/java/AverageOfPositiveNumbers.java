9355866e7fdb5b8410ca81cea0beb96dc0a32995
Vikas <jhavikas2004@gmail.com>
2025-04-03 10:41:36 +0530

    Part 02-13

---
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if ( num == 0) {
+                break;
+            }
+            sum = sum + num;
+        }
+        System.out.println("Sum of the numbers:" + sum);
+    }
+}
